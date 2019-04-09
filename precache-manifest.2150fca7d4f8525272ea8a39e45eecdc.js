342df823766511dd1f80528640425de29d2179be
Nguyen Hieu Thang <nguyenthang1597@gmail.com>
2019-04-09 17:37:34 +0700

    Updates

---
+++ precache-manifest.2150fca7d4f8525272ea8a39e45eecdc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "e51ba764d490abe07446",
+    "url": "/static/js/0.2c7db7fd.chunk.js"
+  },
+  {
+    "revision": "2cc2d9a2f415141c2e3f",
+    "url": "/static/js/main.f3295c35.chunk.js"
+  },
+  {
+    "revision": "9a142c29a04f57cadcb3",
+    "url": "/static/js/runtime~main.9b8230f0.js"
+  },
+  {
+    "revision": "3b5c0b69658eff67456e",
+    "url": "/static/js/3.4635ce7e.chunk.js"
+  },
+  {
+    "revision": "376bd1202ff067778ca2",
+    "url": "/static/css/4.e2fa211e.chunk.css"
+  },
+  {
+    "revision": "376bd1202ff067778ca2",
+    "url": "/static/js/4.29da981f.chunk.js"
+  },
+  {
+    "revision": "9332d8cd0100e28589c5",
+    "url": "/static/js/5.6aba9f62.chunk.js"
+  },
+  {
+    "revision": "1b0d3ded337dddcc5fc2",
+    "url": "/static/css/6.e2fa211e.chunk.css"
+  },
+  {
+    "revision": "1b0d3ded337dddcc5fc2",
+    "url": "/static/js/6.3e5dc124.chunk.js"
+  },
+  {
+    "revision": "a9bb88dc9e1fe6251620",
+    "url": "/static/css/7.e2fa211e.chunk.css"
+  },
+  {
+    "revision": "a9bb88dc9e1fe6251620",
+    "url": "/static/js/7.ee7b679d.chunk.js"
+  },
+  {
+    "revision": "6ef571f9627b085057a3",
+    "url": "/static/js/8.49b2daae.chunk.js"
+  },
+  {
+    "revision": "eb9fd8ac3febb765eb3a",
+    "url": "/static/js/9.08fffadb.chunk.js"
+  },
+  {
+    "revision": "51ed778822ae0bc452c8",
+    "url": "/static/js/10.7b1b2f2e.chunk.js"
+  },
+  {
+    "revision": "dfb3ee3d7aaf418b9186",
+    "url": "/static/js/11.3db1c411.chunk.js"
+  },
+  {
+    "revision": "1a9f549c0e29ceb1e936",
+    "url": "/static/js/12.70cf82c7.chunk.js"
+  },
+  {
+    "revision": "e5faa1e9da0e0925663a",
+    "url": "/static/js/13.8efdfc78.chunk.js"
+  },
+  {
+    "revision": "8da09fef2f5928d5a45e0e6142b31881",
+    "url": "/static/media/product.8da09fef.svg"
+  },
+  {
+    "revision": "a2a3852292223e077fb7206740b8c3d1",
+    "url": "/static/media/services.a2a38522.svg"
+  },
+  {
+    "revision": "7cb838c51bde00ad07f35bd6af5d59b4",
+    "url": "/static/media/banle.7cb838c5.svg"
+  },
+  {
+    "revision": "6ff8b96487c4b16c7fb905655e4e9521",
+    "url": "/static/media/thongke.6ff8b964.svg"
+  },
+  {
+    "revision": "bb372aea48e98146a3f01321deb92bf1",
+    "url": "/static/media/back.bb372aea.svg"
+  },
+  {
+    "revision": "620e8f1f52824a5da3d6f9502945c2fb",
+    "url": "/static/media/staff.620e8f1f.svg"
+  },
+  {
+    "revision": "f08f528380f3ffeb5116a1511b5a5e9c",
+    "url": "/static/media/repairPrice.f08f5283.svg"
+  },
+  {
+    "revision": "5a7a0274091035d1ea0f02ff48c0868b",
+    "url": "/static/media/logout.5a7a0274.svg"
+  },
+  {
+    "revision": "56fb148bcde8a8988917e97e7c32ad28",
+    "url": "/static/media/chamcong.56fb148b.svg"
+  },
+  {
+    "revision": "1ee036157939bf6f73d8d0ae4660df86",
+    "url": "/static/media/customer.1ee03615.svg"
+  },
+  {
+    "revision": "14f250f34edeb8817b48d50b278a68c4",
+    "url": "/static/media/repair.14f250f3.svg"
+  },
+  {
+    "revision": "c8842f0480710301e6fe3b6ae1fbb991",
+    "url": "/index.html"
+  }
+];
